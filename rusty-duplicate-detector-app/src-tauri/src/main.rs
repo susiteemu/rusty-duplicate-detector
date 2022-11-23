797fb3b9c2669069b39d5c9f1deb7c379c497d0a
teemut <teturun@gmail.com>
2022-11-23 21:34:26 +0200

    Playing around with events, part 2

--- rusty-duplicate-detector-app/src-tauri/src/main.rs
+++ rusty-duplicate-detector-app/src-tauri/src/main.rs
@@ -1,10 +1,15 @@
+use serde_json;
 use std::path::PathBuf;
-use std::sync::mpsc::channel;
 use std::{thread, time::Duration};
-use tauri::{Manager, Window};
+use tauri::Manager;
 
 #[derive(Clone, serde::Serialize)]
-struct PathReceived {
+struct PathFoundPayload {
+    path: String,
+}
+
+#[derive(Clone, serde::Deserialize)]
+struct StartWalkPayload {
     path: String,
 }
 
@@ -12,9 +17,8 @@ struct PathReceived {
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn walk(path: &str, window: Window) -> Result<(), ()> {
+
+fn do_walk<R: tauri::Runtime>(path: &str, manager: &impl Manager<R>) {
     println!("Walking {}", path);
     let resource_dir = PathBuf::from(path);
     let walker_rx = mediawalker::start_walking(&resource_dir);
@@ -23,10 +27,10 @@ fn walk(path: &str, window: Window) -> Result<(), ()> {
             Ok(result) => {
                 if result == true {
                     println!("Received path {}", received.path);
-                    window
-                        .emit(
-                            "path-received",
-                            PathReceived {
+                    manager
+                        .emit_all(
+                            "path-found",
+                            PathFoundPayload {
                                 path: received.path,
                             },
                         )
@@ -34,24 +38,25 @@ fn walk(path: &str, window: Window) -> Result<(), ()> {
                     thread::sleep(Duration::from_millis(1000));
                 }
             }
-            Err(err) => eprintln!("Error"),
+            Err(err) => eprintln!("Error {}", err),
         }
     }
     println!("All received");
-    Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let main_window = app.get_window("main").unwrap();
-
-            let id = main_window.listen("walk-started", |event| {
-                println!("{:?}", event.payload().unwrap())
+            // let copy = app.manager.clone();
+            let app_handle = app.handle();
+            let _id = app.listen_global("walk-started", move |event| {
+                let start_walk: StartWalkPayload =
+                    serde_json::from_str(event.payload().unwrap()).unwrap();
+                let path = start_walk.path;
+                do_walk(&path, &app_handle);
             });
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![walk])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- rusty-duplicate-detector-app/src/components/Walk.vue
+++ rusty-duplicate-detector-app/src/components/Walk.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
 import { emit, listen } from '@tauri-apps/api/event'
-import { appWindow, WebviewWindow } from '@tauri-apps/api/window';
 
 
 const path = ref("");
 const walkResponse = ref();
 console.warn(">>> Setup")
 
-const unlisten = appWindow.listen('path-received', (event) => {
+const unlisten = listen('path-found', (event) => {
   console.warn(">> ", event);
   let payload = event.payload as { path: string };
   walkResponse.value = walkResponse.value ? `${walkResponse.value} ${payload.path}` : payload.path;
@@ -17,12 +15,7 @@ const unlisten = appWindow.listen('path-received', (event) => {
 
 
 function walk() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke("walk", { path: path.value })
-      .then((result) => console.info("Walk done"))
-      .catch((error) => console.error(error));
-    const webview = new WebviewWindow('window')
-    webview.emit('walk-started', path.value)
+    emit('walk-started', { path: path.value });
 }
 </script>
 
