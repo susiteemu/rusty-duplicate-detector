b5e95648128d462abc9054a2ca74c2e25a4451ea
teemut <teturun@gmail.com>
2022-11-23 18:36:23 +0200

    Playing around with events

--- rusty-duplicate-detector-app/src-tauri/src/main.rs
+++ rusty-duplicate-detector-app/src-tauri/src/main.rs
@@ -1,5 +1,12 @@
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
+use std::{thread, time::Duration};
+use tauri::{Manager, Window};
+
+#[derive(Clone, serde::Serialize)]
+struct PathReceived {
+    path: String,
+}
 
 #[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
@@ -7,28 +14,43 @@ use std::sync::mpsc::channel;
 )]
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn walk(path: &str) -> Result<Vec<String>, String> {
+fn walk(path: &str, window: Window) -> Result<(), ()> {
     println!("Walking {}", path);
     let resource_dir = PathBuf::from(path);
-    let mut results = Vec::new();
     let walker_rx = mediawalker::start_walking(&resource_dir);
     for received in walker_rx {
         match received.result {
             Ok(result) => {
                 if result == true {
                     println!("Received path {}", received.path);
-                    results.push(received.path)
+                    window
+                        .emit(
+                            "path-received",
+                            PathReceived {
+                                path: received.path,
+                            },
+                        )
+                        .unwrap();
+                    thread::sleep(Duration::from_millis(1000));
                 }
             }
             Err(err) => eprintln!("Error"),
         }
     }
     println!("All received");
-    Ok(results)
+    Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+
+            let id = main_window.listen("walk-started", |event| {
+                println!("{:?}", event.payload().unwrap())
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![walk])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- rusty-duplicate-detector-app/src/components/Walk.vue
+++ rusty-duplicate-detector-app/src/components/Walk.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { emit, listen } from '@tauri-apps/api/event'
+import { appWindow, WebviewWindow } from '@tauri-apps/api/window';
+
 
 const path = ref("");
 const walkResponse = ref();
+console.warn(">>> Setup")
 
-async function walk() {
+const unlisten = appWindow.listen('path-received', (event) => {
+  console.warn(">> ", event);
+  let payload = event.payload as { path: string };
+  walkResponse.value = walkResponse.value ? `${walkResponse.value} ${payload.path}` : payload.path;
+})
+
+
+function walk() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  invoke("walk", { path: path.value })
-    .then((result) => walkResponse.value = result)
-    .catch((error) => console.error(error));
+    invoke("walk", { path: path.value })
+      .then((result) => console.info("Walk done"))
+      .catch((error) => console.error(error));
+    const webview = new WebviewWindow('window')
+    webview.emit('walk-started', path.value)
 }
 </script>
 
